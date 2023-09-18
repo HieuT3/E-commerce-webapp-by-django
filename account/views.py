e3a0dd9cf05f5186c4e5638b834564377e519dae
HieuT3 <hieucao135kk@gmail.com>
2023-09-18 15:06:55 +0700

    New Project

---
+++ account/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+
+from .forms import CreateUserForm
+
+from django.contrib.sites.shortcuts import get_current_site
+from . token import user_tokenizer_generate
+
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
+
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+
+            user.is_active = False
+
+            user.save()
+
+            # Email verification setup template
+
+            current_site = get_current_site(request)
+
+            subject = 'Account verification email'
+
+            message = render_to_string('account/register/email-verification.html', {
+
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user)
+
+            })
+
+            user.email_user(subject=subject, message=message)
+
+            return redirect('email-verification-sent')
+
+            
+        
+    context = {'form' : form}
+
+    return render(request, 'account/register/register.html', context)
+
+def email_verification(request):
+
+    pass
+
+def email_verification_sent(request):
+
+    pass
+
+def email_verification_success(request):
+
+    pass
+
+def email_verification_failed(request):
+
+    pass
