d6630141472300a532ca226c02447365848d154f
HieuT3 <hieucao135kk@gmail.com>
2023-09-18 22:41:37 +0700

    Change2

--- account/views.py
+++ account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 
 from .forms import CreateUserForm
 
+from django.contrib.auth.models import User
+
 from django.contrib.sites.shortcuts import get_current_site
 from . token import user_tokenizer_generate
 
@@ -51,18 +53,32 @@ def register(request):
 
     return render(request, 'account/register/register.html', context)
 
-def email_verification(request):
+def email_verification(request, uidb64, token):
 
-    pass
+    unique_id = force_str(urlsafe_base64_decode(uidb64))
+
+    user = User.objects.get(pk=unique_id)
+
+    if user and user_tokenizer_generate.check_token(user, token):
+
+        user.is_active = True
+
+        return redirect('email-verification-success')
+    
+    else:
+
+        return redirect('email-verificartion-failed')
+
+
 
 def email_verification_sent(request):
 
-    pass
+    return render(request, 'account/register/email-verification-sent.html')
 
 def email_verification_success(request):
 
-    pass
+    return render(request, 'account/register/email-verification-success.html')
 
 def email_verification_failed(request):
 
-    pass
+    return render(request, 'account/register/email-verification-failed.html')
