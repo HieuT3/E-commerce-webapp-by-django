c728dafa6a9740d112ae84273761e555da19b67f
HieuT3 <hieucao135kk@gmail.com>
2023-09-20 15:49:58 +0700

    Chang 10

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
 
     # Register
@@ -31,5 +33,23 @@ urlpatterns = [
     path('profile-management', views.profile_management, name='profile-management'),
 
     path('delete-account', views.delete_account, name='delete-account'),
+
+    # Reset password
+
+    # 1. Submit our email form
+
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name='account/password/password-reset.html'), name='reset_password'),
+
+    # 2. Success message stating that password reset enail was sent
+
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name='account/password/password-reset-sent.html'), name='password_reset_done'),
+
+    # 3. Password reset link
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/password/password-reset-form.html'), name='password_reset_confirm'),
+
+    # 4. Success message stating that our password was reset
+
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name='account/password/password-reset-complete.html'), name='password_reset_complete'),
 
 ]
