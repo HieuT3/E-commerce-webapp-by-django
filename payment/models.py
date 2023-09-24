2f803bd5d43cca27d4b19a67f4667153ab90502d
HieuT3 <hieucao135kk@gmail.com>
2023-09-24 22:32:28 +0700

    Change 15

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class ShippingAddress(models.Model):
+
+    full_name = models.CharField(max_length=255)
+
+    email = models.EmailField(max_length=255)
+
+    address1 = models.CharField(max_length=255)
+
+    address2 = models.CharField(max_length=255)
+
+    city = models.CharField(max_length=255)
+
+    # Optional
+
+    state = models.CharField(max_length=255, null=True, blank=True)
+
+    zipcode = models.CharField(max_length=255, null=True, blank=True)
+
+    # Foreign Key
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+
+        verbose_name_plural = 'Shipping Address'
+
+    def __str__(self):
+
+        return 'Shipping Address - ' + str(self.id)
+    
