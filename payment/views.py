1a76de084c8f78e152e6aa09a3522b88d076dcd2
HieuT3 <hieucao135kk@gmail.com>
2023-09-25 18:23:24 +0700

    update checkout

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-from . models import ShippingAddress
+from . models import ShippingAddress, Order, OrderItem
+
+from cart.cart import Cart
+
+from django.http import JsonResponse
 
 def checkout(request):
 
@@ -27,10 +31,74 @@ def checkout(request):
 
 def complete_order(request):
 
-    pass
+    if request.POST.get('action') == 'post':
+
+        name = request.POST.get('name')
+
+        email = request.POST.get('email')
+
+        address1 = request.POST.get('address1')
+
+        address2 = request.POST.get('address2')
+
+        city = request.POST.get('city')
+
+        state = request.POST.get('state')
+
+        zipcode = request.POST.get('zipcode')
+
+        shipping_address = (address1 + '\n' + address2 + '\n' + city + '\n' + state + '\n' + zipcode)
+
+        cart = Cart(request)
+
+        total_cost = cart.get_total()
+
+        # Order variations: cac bien the cua order
+
+        # 1. Create order: account users with + without shipping infomation
+
+        # 2. Create order: Guest users without an account
+
+        if request.user.is_authenticated:
+
+            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address, amount_paid=total_cost, user=request.user)
+
+            order_id = order.pk
+
+            for item in cart:
+
+                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'], price=item['price'], user=request.user)
+
+        else:
+
+            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address, amount_paid=total_cost)
+
+            order_id = order.pk
+
+            for item in cart:
+
+                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'], price=item['price'])
+
+        
+        order_success = True
+
+        response = JsonResponse({'success': order_success})
+
+        return response
+
+
+
+
+
 
 
 def payment_success(request):
+
+    for key in list(request.session.keys()):
+
+        if key == 'session_key':
+
+            del request.session[key]
 
     return render(request, 'payment/payment-success.html')
 
